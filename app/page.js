83d54acf0f755a223702b5ff819ba71de800a675
Vladislav Pavlov <vldslw@gmail.com>
2023-11-16 01:23:15 -0300

    feat: change env

--- app/page.js
+++ app/page.js
@@ -1,8 +1,26 @@
 import { Cover } from "@/components/Cover/Cover";
 import { Stack } from "@/components/Stack/Stack";
-import { getLandingData } from "@/services/getData";
+// import { getLandingData } from "@/services/getData";
+
+export const getLandingData = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/landing`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    },
+  );
+
+  if (!response.ok) throw new Error("Unable to fetch posts.");
+
+  return response.json();
+};
 
 export default async function Home() {
+  if (!process.env.NEXT_PUBLIC_BASE_API_URL) {
+    return null;
+  }
   const landingData = (await getLandingData())[0];
 
   return (
--- services/getData.js
+++ services/getData.js
@@ -1,9 +1,12 @@
 export const getLandingData = async () => {
-  const response = await fetch(`${process.env.BASE_URL}/api/landing`, {
-    next: {
-      revalidate: 60,
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/landing`,
+    {
+      next: {
+        revalidate: 60,
+      },
     },
-  });
+  );
 
   if (!response.ok) throw new Error("Unable to fetch posts.");
 
