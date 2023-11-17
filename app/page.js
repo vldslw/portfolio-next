72d976489021a91ef1a44f347c25f914999a5397
Vladislav Pavlov <vldslw@gmail.com>
2023-11-17 14:54:13 -0300

    feat: add projects section and api

--- app/page.js
+++ app/page.js
@@ -1,19 +1,23 @@
 import { Cover } from "@/components/Cover/Cover";
+import { Projects } from "@/components/Projects/Projects";
 import { Skills } from "@/components/Skills/Skills";
 import { Stack } from "@/components/Stack/Stack";
 import { getLandingData } from "@/services/getData";
+import { getProjects } from "@/services/getData";
 
 export default async function Home() {
   if (!process.env.NEXT_PUBLIC_BASE_API_URL) {
     return null;
   }
   const landingData = (await getLandingData())[0];
+  const projects = await getProjects();
 
   return (
     <main>
       <Cover data={landingData} />
       <Stack data={landingData} />
       <Skills data={landingData} />
+      <Projects data={projects} />
     </main>
   );
 }
--- services/getData.js
+++ services/getData.js
@@ -12,3 +12,18 @@ export const getLandingData = async () => {
 
   return response.json();
 };
+
+export const getProjects = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/projects`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    },
+  );
+
+  if (!response.ok) throw new Error("Unable to fetch posts.");
+
+  return response.json();
+};
